3b33c960a9ca0d3564d61f1d713844b3dec9e341
SheepTeacher <NanGua_GG@outlook.com>
2026-07-19 19:42:45 +0800

    更改了运行时间

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -1,13 +1,13 @@
 import type { SiteConfig } from "../types/config";
 
 // 定义站点语言
-const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
+const SITE_LANG = "en"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 
 export const siteConfig: SiteConfig = {
   title: "BG7NTZ",
   subtitle: "One demo website",
   siteURL: "https://blog.sheepteachers.top", // 请替换为你的站点URL，以斜杠结尾
-  siteStartDate: "2026-07-18", // 站点开始运行日期，用于站点统计组件计算运行天数
+  siteStartDate: "2024-12-01", // 站点开始运行日期，用于站点统计组件计算运行天数
 
   lang: SITE_LANG,
 
